27a869e5bf3ff3fa5bb9b97dfe395759ad28bc53
Karan Kadam <72716788+karankadamCode@users.noreply.github.com>
2024-01-24 16:20:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,5 +40,5 @@ def submit_form():
         return 'Something went wrong!'
 
 
-if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=False,host='0.0.0.0')
